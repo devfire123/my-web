4ffd46c562a5fedb0ea95ce4063fc9360f877de4
devfire123 <itxdevil269@gmail.com>
2025-11-28 13:04:50 +0500

    Add full

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import { useEffect, useState } from 'react';
 import Navbar from './components/Navbar';
